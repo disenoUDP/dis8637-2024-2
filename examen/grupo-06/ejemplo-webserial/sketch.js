2f657b4b22a307b57c77fe238e1e33853dda0991
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-12-03 18:53:36 -0300

    cambiar botones a espanol y al html

--- examen/grupo-06/ejemplo-webserial/sketch.js
+++ examen/grupo-06/ejemplo-webserial/sketch.js
@@ -1,33 +1,32 @@
 // This example is also available online in the p5.js web editor:
 // https://editor.p5js.org/gohai/sketches/X0XD9xvIR
 
-let port;
-let connectBtn;
+let puerto;
+let botonConexion;
+let botonEnviar;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
 
-  port = createSerial();
+  puerto = createSerial();
 
   // in setup, we can open ports we have used previously
   // without user interaction
 
-  let usedPorts = usedSerialPorts();
-  if (usedPorts.length > 0) {
-    port.open(usedPorts[0], 57600);
+  let puertosUsados = usedSerialPorts();
+  if (puertosUsados.length > 0) { 
+    puerto.open(puertosUsados[0], 57600);
   }
+
+  botonConexion = document.getElementById("botonConexion");
+  botonConexion.addEventListener("click", conectarClick);
 
   // any other ports can be opened via a dialog after
   // user interaction (see connectBtnClick below)
 
-  connectBtn = createButton('Connect to Arduino');
-  connectBtn.position(80, 200);
-  connectBtn.mousePressed(connectBtnClick);
-
-  let sendBtn = createButton('Send hello');
-  sendBtn.position(220, 200);
-  sendBtn.mousePressed(sendBtnClick);
+  botonEnviar = document.getElementById("botonEnviar");
+  botonEnviar.addEventListener("click", enviarClick);
 }
 
 function draw() {
@@ -36,27 +35,27 @@ function draw() {
 
   // reads in complete lines and prints them at the
   // bottom of the canvas
-  let str = port.readUntil('\n');
+  let str = puerto.readUntil('\n');
   if (str.length > 0) {
     text(str, 10, height - 20);
   }
 
   // changes button label based on connection status
-  if (!port.opened()) {
-    connectBtn.html('Connect to Arduino');
+  if (!puerto.opened()) {
+    botonConexion.innerHTML = "Conectar";
   } else {
-    connectBtn.html('Disconnect');
+    botonConexion.innerHTML = "Desconectar";
   }
 }
 
-function connectBtnClick() {
-  if (!port.opened()) {
-    port.open('Arduino', 57600);
+function conectarClick() {
+  if (!puerto.opened()) {
+    puerto.open("Arduino", 57600);
   } else {
-    port.close();
+    puerto.close();
   }
 }
 
-function sendBtnClick() {
-  port.write('Hello from p5.js\n');
+function enviarClick() {
+  puerto.write("Hola desde p5.js\n");
 }
