9e9e8b25f0775dcc6080138e8bc0a0a12f4a680f
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-12-03 18:37:13 -0300

    agregar ejemplo

--- examen/grupo-06/ejemplo-webserial/sketch.js
+++ examen/grupo-06/ejemplo-webserial/sketch.js
@@ -3,7 +3,10 @@ let botonEnviar;
 
 let puerto;
 
-
+let puertosUsados = usedSerialPorts();
+if (puertosUsados.length > 0) {
+  port.open(usedPorts[0], 9600);
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -18,12 +21,29 @@ function setup() {
 
 function draw() {
   background(255, 0, 0);
+
+  copy(0, 0, width, height, 0, -1, width, height);
+
+  let str = port.readUntil("\n");
+  if (str.length > 0) {
+    text(str, 10, height-20);
+  }
+
+  if (!puerto.opened()) {
+    botonConectar.innerHTML = 'Conectar';
+  } else {
+    botonConectar.innerHTML = 'Desconectar';
+  }
 }
 
 function conectar () {
-  puerto.open(9600);
+  if (!puerto.opened()) {
+    puerto.open('Arduino', 9600);
+  } else {
+    puerto.close();
+  }
 }
 
 function enviarHola () {
-  puerto.write('hola\n');
+  puerto.write("hola\n");
 }
