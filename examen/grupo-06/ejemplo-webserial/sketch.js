1683aeeab0fb7fca74bcfa2f88de96eb3bfe996d
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-12-03 18:26:42 -0300

    agregar puerto

--- examen/grupo-06/ejemplo-webserial/sketch.js
+++ examen/grupo-06/ejemplo-webserial/sketch.js
@@ -1,7 +1,29 @@
+let botonConectar;
+let botonEnviar;
+
+let puerto;
+
+
+
 function setup() {
   createCanvas(400, 400);
+  puerto = createSerial();
+
+  botonConectar = document.getElementById('botonConectar');
+  botonConectar.addEventListener('click', conectar);
+
+  botonEnviar = document.getElementById('botonEnviar');
+  botonEnviar.addEventListener('click', enviarHola);
 }
 
 function draw() {
-  background(220);
+  background(255, 0, 0);
+}
+
+function conectar () {
+  puerto.open(9600);
+}
+
+function enviarHola () {
+  puerto.write('hola');
 }
